1fa0d52fc01d1961c57cd1cb0c57d8e61738f9d5
eleqtron <eleqtron@yandex.ru>
2014-01-01 23:23:34 +0400

    config init

--- protected/config/main.php
+++ protected/config/main.php
@@ -37,7 +37,7 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -46,12 +46,13 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
+		*/
 		// uncomment the following to use a MySQL database
-		/*
+
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
@@ -59,7 +60,7 @@ return array(
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -79,6 +80,10 @@ return array(
 				*/
 			),
 		),
+        'cache'=>array(
+            'class'=>'system.caching.CDummyCache',
+            //'class'=>'system.caching.CFileCache',
+        ),
 	),
 
 	// application-level parameters that can be accessed
